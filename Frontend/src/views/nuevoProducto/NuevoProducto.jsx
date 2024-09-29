73b4a830f08ce225fa0a4130ec4d333f70e14c7e
darkfuel <44975473+darkfuel@users.noreply.github.com>
2024-09-29 20:56:48 -0300

    Update NuevoProducto.jsx

--- Frontend/src/views/nuevoProducto/NuevoProducto.jsx
+++ Frontend/src/views/nuevoProducto/NuevoProducto.jsx
@@ -72,81 +72,73 @@ const NuevoProducto = () => {
 
   return (
     <>
-      <div className='container'>
-        <div className='row'>
-          <h2 className='text-center mt-2'>Agrega un nuevo producto:</h2>
+      <h2 className='tituloNuevoProducto'>Agrega un nuevo producto:</h2>
+      <form className='formNuevoProducto' onSubmit={handleForm}>
+        <div className='producto d-flex flex-column'>
+          <img
+            id='imgNuevoProducto'
+            className='imgNuevoProducto'
+            src={imgSrc}
+            alt='agregar nuevo producto'
+          />
+          <input
+            type='text'
+            className='form-control'
+            placeholder='Buscar productos...'
+            value={imgSrc}
+            onChange={handleChange}
+          />
         </div>
 
-        <form className='formNuevoProducto' onSubmit={handleForm}>
-          <div className='form-row'>
-
-            <div className='form-group col mb-4'>
-              <img
-                id='imgNuevoProducto'
-                className='img-fluid'
-                src={imgSrc}
-                alt='agregar nuevo producto'
-              />
-              <input
-                type='text'
-                className='form-control mt-3'
-                placeholder='Buscar productos...'
-                value={imgSrc}
-                onChange={handleChange}
-              />
-            </div>
-
-            <div className='form-group col'>
-              <MDBInput
-                type='text'
-                id='NombreProducto'
-                name='nombre'
-                wrapperClass='mb-4'
-                label='Nombre del producto'
-                value={producto.nombre}
-                onChange={handleProduct}
-              />
-              <div className='d-flex gap-3'>
-                <MDBInput
-                  type='number'
-                  id='precioProducto'
-                  name='precio'
-                  wrapperClass='mb-4'
-                  label='Precio'
-                  value={producto.precio}
-                  onChange={handleProduct}
-                />
-                <MDBInput
-                  type='number'
-                  id='stock'
-                  name='stock'
-                  wrapperClass='mb-4'
-                  label='Stock'
-                  value={producto.stock}
-                  onChange={handleProduct}
-                />
-              </div>
-
-              <MDBInput
-                wrapperClass='mb-4'
-                type='text'
-                className='descripcionProducto'
-                id='descripcionProducto'
-                name='descripcion'
-                rows={4}
-                label='Descripción'
-                value={producto.descripcion}
-                onChange={handleProduct}
-              />
-
-              <MDBBtn type='submit' className='mb-4' block>
-                Agregar Producto
-              </MDBBtn>
-            </div>
+        <div className='detalleProducto'>
+          <MDBInput
+            type='text'
+            id='NombreProducto'
+            name='nombre'
+            wrapperClass='mb-4'
+            label='Nombre del producto'
+            value={producto.nombre}
+            onChange={handleProduct}
+          />
+          <div className='d-flex gap-3'>
+            <MDBInput
+              type='number'
+              id='precioProducto'
+              name='precio'
+              wrapperClass='mb-4'
+              label='Precio'
+              value={producto.precio}
+              onChange={handleProduct}
+            />
+            <MDBInput
+              type='number'
+              id='stock'
+              name='stock'
+              wrapperClass='mb-4'
+              label='Stock'
+              value={producto.stock}
+              onChange={handleProduct}
+            />
           </div>
-        </form>
-      </div>
-    </>
+
+          <MDBInput
+            wrapperClass='mb-4'
+            type='text'
+            className='descripcionProducto'
+            id='descripcionProducto'
+            name='descripcion'
+            rows={4}
+            label='Descripción'
+            value={producto.descripcion}
+            onChange={handleProduct}
+          />
+
+          <MDBBtn type='submit' className='mb-4' block>
+            Agregar Producto
+          </MDBBtn>
+        </div>
+      </form>
+    </>
   )
 }
 
