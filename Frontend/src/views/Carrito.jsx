1b296f6301c89fada43fb0cc5c8db3d8fedb0806
jpgit01 <33032727+jpgit01@users.noreply.github.com>
2024-09-29 08:25:54 -0600

    Update Carrito.jsx

--- Frontend/src/views/Carrito.jsx
+++ Frontend/src/views/Carrito.jsx
@@ -14,49 +14,47 @@ const Carrito = () => {
   )
   setTotal(totalCart)
 
+  const displayClass = totalCart === 0 ? 'd-none' : 'd-block col-md-6 text-center mb-3';
+  const displayClassNone = totalCart === 0 ? 'd-block col-md-12 text-center mb-3' : 'd-none';
+
   return (
     <Container>
       <Row>
-        <Col><h5 className='m-5'>Detalle del Pedido:</h5></Col>
+        <Col><h5 className='m-5 text-center'>Detalle del Pedido:</h5></Col>
       </Row>
       <Row>
-        <div className='col-md-8'>
+        <div className={displayClass}>
           <div className='w-100 shadow-lg p-5 mb-3 bg-body-tertiary rounded mb-5'>
 
           {cart.map((producto, index) => (
-            <div key={producto.id} className='border rounded bg-light mb-3 text-center'>
-              <div className='col-md-12'>
-              <h5 className='mt-3'>
-                  <strong>{producto.nombre}</strong>
-                </h5>
-                <img src={producto.img === '' ? imgSrc : producto.img} alt={producto.nombre} width="70%" className='p-3 shadow border' />
+            <div key={producto.id} className='bg-light mb-3 text-center'>
+  <div className='d-flex flex-column flex-md-row justify-content-between align-items-center'>
+    <div className='col-md-6'>
+      <h5 className='mt-3'>
+        <strong>{producto.nombre}</strong>
+      </h5>
+      <img src={producto.img === '' ? imgSrc : producto.img} alt={producto.nombre} width="70%" className='p-2 shadow border' />
+    </div>
+    <div className='col-md-6'>
+      <p>Precio: {producto.precio}</p>
+      <div className='d-flex justify-content-center align-items-center gap-3 text-center'>
+        <button className='btn btn-danger' onClick={() => donwCount(index)}>
+          -
+        </button>
+        <p size='5' aria-label='size 4 select example'>{producto.count}</p>
+        <button className='btn btn-success' onClick={() => upCount(index)}>
+          +
+        </button>
+      </div>
+    </div>
+  </div>
+  <hr />
+</div>
 
-              </div>
-              <div className='col-md-12'>
-                <p>Precio : {producto.precio}</p>
-
-                <div className='d-flex justify-content-center align-items-center gap-3 text-center'>
-                  <button
-                    className='btn btn-danger '
-                    onClick={() => donwCount(index)}
-                  >
-                    -
-                  </button>
-                  <p size='5' aria-label='size 4 select example'>{producto.count}</p>
-                  <button
-                    className='btn btn-success'
-                    onClick={() => upCount(index)}
-                  >
-                    +
-                  </button>
-                </div>
-              </div>
-              <hr />
-            </div>
           ))}
           </div>
         </div>
-        <div className='col-md-4 text-center mb-3'>
+        <div className={displayClass}>
           <h2 className='mb-3'>Total: {total}</h2>
           <button className='btn btn-secondary me-3' onClick={() => eraseCart()}>
             Limpiar
@@ -66,6 +64,9 @@ const Carrito = () => {
           </button>
           <hr />
           <Payment eraseCart={eraseCart} />
+        </div>
+        <div className={displayClassNone}>
+          <h3> No hay productos agregados</h3>
         </div>
     </Row>
     </Container>
